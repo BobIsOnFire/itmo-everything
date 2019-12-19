922a86a52c7c331426e0280506858e8305aac97a
BobIsNotOnFire <frfntdakatevin@gmail.com>
2019-12-19 23:04:04 +0300

    (web-programming/spring-and-react) initial commit

---
+++ web-programming/spring-and-react/src/main/java/com/bobisonfire/lab4/User.java
@@ -0,0 +1,49 @@
+package com.bobisonfire.lab4;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id @GeneratedValue
+    private long id;
+
+    private String userName;
+    private String password;
+
+    public User() {
+    }
+
+    public User(String userName, String password) {
+        this.userName = userName;
+        this.password = password;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
