c3c35c2a0fe357dfa0007c54328c7107f0e3e385
BobIsNotOnFire <frfntdakatevin@gmail.com>
2020-04-25 16:40:01 +0500

    (software-engineering/ant-junit) fix test coverage and add dustin.jpg

--- software-engineering/ant-junit/src/main/test/com/bobisonfire/web/beans/HistoryNodeTest.java
+++ software-engineering/ant-junit/src/main/test/com/bobisonfire/web/beans/HistoryNodeTest.java
@@ -23,76 +23,105 @@ public class HistoryNodeTest {
     }
 
     @Test
-    public void test_node_top_left() {
+    public void test_node_zero() {
         node.setX(BigDecimal.ZERO);
         node.setY(BigDecimal.ZERO);
         assertTrue(node.isHit());
+    }
 
+    @Test
+    public void test_node_far_top_left() {
         node.setX(BigDecimal.ONE.negate());
         node.setY(BigDecimal.ONE);
-        assertFalse(node.isHit());
-
-        node.setX(new BigDecimal("-0.000000000000000000001"));
-        node.setY(new BigDecimal("0.00000000000000000001"));
-        assertFalse(node.isHit());
-
-        node.setX(BigDecimal.valueOf(-0.5d));
-        node.setY(BigDecimal.valueOf(0.5d));
         assertFalse(node.isHit());
     }
 
     @Test
-    public void test_node_top_right() {
+    public void test_node_top_left_near_zero() {
+        node.setX(new BigDecimal("-0.000000000000000000001"));
+        node.setY(new BigDecimal("0.00000000000000000001"));
+        assertFalse(node.isHit());
+    }
+
+    @Test
+    public void test_node_far_right() {
         node.setX(BigDecimal.ONE);
         node.setY(BigDecimal.ZERO);
         assertTrue(node.isHit());
+    }
 
-        node.setX(BigDecimal.ZERO);
-        node.setY(BigDecimal.ONE);
+    @Test
+    public void test_node_circle_in_bound() {
+        node.setX(BigDecimal.valueOf(0.5d));
+        node.setY(BigDecimal.valueOf(0.5d));
         assertTrue(node.isHit());
+    }
 
+    @Test
+    public void test_node_circle_on_bound() {
         node.setX(BigDecimal.valueOf(0.6d));
         node.setY(BigDecimal.valueOf(0.8d));
         assertTrue(node.isHit());
+    }
 
+    @Test
+    public void test_node_circle_slightly_off_bound() {
         node.setX(new BigDecimal("0.60000000000000000000000001"));
         node.setY(BigDecimal.valueOf(0.8d));
         assertFalse(node.isHit());
     }
 
     @Test
-    public void test_node_bottom_left() {
+    public void test_node_rectangle_in_bound() {
         node.setX(BigDecimal.valueOf(-0.25d));
         node.setY(BigDecimal.valueOf(-0.5d));
         assertTrue(node.isHit());
+    }
 
+    @Test
+    public void test_node_rectangle_on_bound() {
         node.setX(BigDecimal.valueOf(-0.5d));
         node.setY(BigDecimal.ONE.negate());
         assertTrue(node.isHit());
+    }
 
+    @Test
+    public void test_node_rectangle_slightly_off_bound() {
         node.setX(BigDecimal.ZERO);
         node.setY(new BigDecimal("-1.00000000000000000001"));
-        assertFalse(node.isHit());
-
-        node.setX(BigDecimal.valueOf(-0.5d));
-        node.setY(new BigDecimal("0.00000000000000000001"));
         assertFalse(node.isHit());
     }
 
     @Test
-    public void test_node_bottom_right() {
+    public void test_node_rectangle_off_bound() {
+        node.setX(BigDecimal.ONE.negate());
+        node.setY(BigDecimal.ONE.negate());
+        assertFalse(node.isHit());
+    }
+
+    @Test
+    public void test_node_triangle_in_bound() {
         node.setX(BigDecimal.valueOf(0.1d));
         node.setY(BigDecimal.valueOf(-0.1d));
         assertTrue(node.isHit());
+    }
 
+    @Test
+    public void test_node_triangle_off_bound() {
         node.setX(BigDecimal.valueOf(0.5d));
         node.setY(BigDecimal.valueOf(-0.1d));
         assertFalse(node.isHit());
+    }
 
+    @Test
+    public void test_node_triangle_on_bound() {
         node.setX(BigDecimal.valueOf(0.25d));
         node.setY(BigDecimal.valueOf(-0.25d));
         assertTrue(node.isHit());
+    }
 
+    @Test
+    public void test_node_triangle_slightly_off_bound() {
         node.setX(BigDecimal.valueOf(0.25d));
         node.setY(new BigDecimal("-0.2500000000000000000001"));
         assertFalse(node.isHit());
